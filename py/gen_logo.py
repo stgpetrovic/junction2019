70564dddb48bb1c3aff45d43f13155545518709a
Bruno Rahle <brahle@gmail.com>
2019-11-17 01:29:46 +0200

    Generacija logo.svg

--- py/gen_logo.py
+++ py/gen_logo.py
@@ -2,11 +2,13 @@ from typing import Dict
 
 SVG = """
 <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  {BASKET}
-  {EYES}
-  {MOUTH}
+  {basket}
+  {eyes}
+  {mouth}
 </svg>
 """
+
+DOMAIN = "https://green-basket.xyz/"
 
 class IncludeImage():
     def __init__(self: 'IncludeImage', href: str, x: float, y: float, height: float, width: float) -> None:
@@ -19,11 +21,11 @@ class IncludeImage():
 
     def format(self: 'IncludeImage') -> str:
         return '<image xlink:href="{href}" x="{x}" y="{y}" height="{height}" width="{width}"/>'.format(
-            href = self.href,
-            x = self.x,
-            y = self.y,
-            height = self.height,
-            width = self.width,
+            href=DOMAIN + self.href,
+            x=self.x,
+            y=self.y,
+            height=self.height,
+            width=self.width,
         )
 
     def to_dict(self: 'IncludeImage') -> Dict[str, str]:
@@ -43,19 +45,27 @@ def logo(health: float, sustinable: float) -> Dict[str, Dict[str, str]]:
         "mouth": mouth(health, sustinable).to_dict(),
     }
 
+def logo_svg(health: float, sustainable: float) -> str:
+    return SVG.format(
+        basket=basket(health, sustainable).format(),
+        eyes=eyes(health, sustainable).format(),
+        mouth=mouth(health, sustainable).format(),
+    )
+
+
 def basket(health:float, sustinable: float) -> IncludeImage:
     return IncludeImage("images/basket.png", 0, 0, 300, 300)
 
 def eyes(health:float, sustinable: float) -> IncludeImage:
     if health < 33:
-        return IncludeImage("images/angry-eyes.png", 95, 150, 75, 125)
+        return IncludeImage("images/angry-eyes.png", 90, 150, 75, 125)
     if health < 66:
-        return IncludeImage("images/concerened-eyes.png", 95, 155, 75, 125)
-    return IncludeImage("images/happy-eyes.png", 95, 150, 75, 125)
+        return IncludeImage("images/concerened-eyes.png", 90, 155, 75, 125)
+    return IncludeImage("images/happy-eyes.png", 90, 150, 75, 125)
 
 def mouth(health:float, sustinable: float) -> IncludeImage:
     if sustinable < 33:
-        return IncludeImage("images/shocked-mouth.png", 33, 225, 60, 250)
+        return IncludeImage("images/shocked-mouth.png", 28, 225, 60, 250)
     if sustinable < 66:
-        return IncludeImage("images/happy-mouth.png", 70, 225, 50, 175)
-    return IncludeImage("images/tasty-mouth.png", 30, 225, 60, 250)
+        return IncludeImage("images/happy-mouth.png", 65, 225, 50, 175)
+    return IncludeImage("images/tasty-mouth.png", 25, 225, 60, 250)
