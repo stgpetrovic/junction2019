0309752b8a4ca875466439c961e29bb802b52cab
lstrz <lstrz@users.noreply.github.com>
2019-11-16 13:37:58 +0100

    Makes popup pop up or not.
    Signed-off-by: lstrz <lstrz@users.noreply.github.com>

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -82,7 +82,6 @@ class Popup {
         this.popupText.className += " green-basket-popup-text";
         this.popupText.style.position = "relative";
         this.popupText.style.padding = "15px";
-        // this.popupText.style.margin = "1em 0 3em";
         this.popupText.style.border = "5px solid #5a8f00";
         this.popupText.style.color = "#333";
         this.popupText.style.background = "#fff";
@@ -91,8 +90,22 @@ class Popup {
     }
 
     setText(txt) {
-        console.log(`Setting the popup text to ${txt}`);
-        this.popupText.innerText = txt;
+        if(txt.length == 0) {
+            console.log('Removing popup text.');
+            this.popupText.style.display = "none";
+        } else {
+            console.log(`Setting the popup text to ${txt}`);
+            this.popupText.innerText = txt;
+            this.popupText.style.display = "block";
+        }
+    }
+
+    decideDisplay(score) {
+        if(score >= 70) {
+            this.setText('Good job!');
+            return;
+        }
+        this.setText('Bad job!');
     }
 };
 
@@ -175,7 +188,10 @@ function main() {
             },
         )
         .then(response => response.json())
-        .then(json => widget.healthyBar.setFill(json.score))
+        .then(json => {
+            widget.healthyBar.setFill(json.score);
+            widget.popup.decideDisplay(json.score);
+        })
         .catch(reason => console.log(reason));
     }
 }
