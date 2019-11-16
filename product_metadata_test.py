f8431e9ed910b3d0c8c3778e6918eecdbef1dd29
Goran Petrovic <stgpetrovic@gmail.com>
2019-11-16 04:08:38 +0100

    Lazanje test

--- product_metadata_test.py
+++ product_metadata_test.py
@@ -5,11 +5,13 @@ import json
 class TestSlonApi(unittest.TestCase):
     def test_info(self):
         m = product_metadata.ProductMetadata()
-        products = m.Infos(['6415600501842','2000158300005'])
+        products = m.Infos(['6415600501842','6417700050527'])
         self.assertEqual(2, len(products))
         kola = products[0]
+        lazanje = products[1]
         self.assertEqual(10.6, kola.sugar_per_100)
         self.assertEqual(42, kola.kcal_per_100g)
+        self.assertEqual(357, lazanje.kcal_per_100g)
 
 if __name__ == '__main__':
     unittest.main()
